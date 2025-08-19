987ef1e797a0b5e8c19cc63a731f64587292511c
eng-mostafa-sayed <ms519197@gmail.com>
2025-08-20 00:17:16 +0300

    fetchData

--- index.ts
+++ index.ts
@@ -3,8 +3,20 @@ import express, { Request, Response } from "express";
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello world");
+app.get("/repos/:username", async (req: Request, res: Response) => {
+  try {
+    console.log("Fetching data");
+    const username = req.params.username;
+    const data = await fetch(`https://api.github.com/users/${username}`);
+
+    const userData = await data.json();
+    const repos = Number(userData.public_repos ?? 0);
+
+    res.send(`<h2>${username} has ${repos} public repositories</h2>`);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("<h2>Internal server error</h2>");
+  }
 });
 
 app.listen(PORT, () => {
